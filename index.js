95a8fb2aeb7c0401cfc6ff9139757e7a12af985e
RodolfoAssam <rodozahir.dbd@gmail.com>
2023-08-29 17:21:33 -0500

    Nuevos cambios

---
+++ index.js
@@ -0,0 +1,16 @@
+let numerosenfrances=[
+    "Un",
+    "Deux",
+    "Trois",
+    "Quatre",
+    "Cinq",
+    "Six",
+    "Sept",
+    "Huit",
+    "Neuf",
+    "Dix",
+]
+
+function obtieneNumeros(indice){
+    return numerosenfrances(indice);
+}
